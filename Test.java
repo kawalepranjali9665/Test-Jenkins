c7e8fd8a55cefda0338e44a48cbb7eb230f799b7
kawalepranjali9665 <42933788+kawalepranjali9665@users.noreply.github.com>
2024-05-09 22:14:56 +0530

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,14 @@
+import java.time.LocalDateTime;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("Hello My JENKINS project to access from git");
+		System.out.println("Today's Date and Time : " + LocalDateTime.now());
+		int a = 10;
+		int b = 20;
+		System.out.println("sum : " + a + b);
+		System.out.println("New Changes for jenkins");
+	}
+
+}
